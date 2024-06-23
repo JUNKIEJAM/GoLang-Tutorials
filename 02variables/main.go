41c606a7acd71e72a6445bfece07e7de7e31d630
Prithesh Dwivedi <66085589+JUNKIEJAM@users.noreply.github.com>
2024-06-24 00:53:04 +0530

    Variables

--- 02variables/main.go
+++ 02variables/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Variables")
+	var username string = "prithesh"
+	fmt.Println(username)
+	fmt.Printf("Variable is of type : %T \n", username)
+
+	var isLoggedIn bool = true
+	fmt.Println(isLoggedIn)
+	fmt.Printf("Variable is of type : %T \n", isLoggedIn)
+
+	var smallFloat float64 = 255.34565432345
+	fmt.Println(smallFloat)
+	fmt.Printf("Variable is of type : %T \n", smallFloat)
+
+	// default values and some aliases
+	var anotherVariable int
+	fmt.Println(anotherVariable)
+	fmt.Printf("Variable is of type : %T \n", anotherVariable)
+
+	// implicity type
+	var website = "wwww.google.com"
+	fmt.Println((website))
+
+	// no var style
+	numberOfUser := 300000.00
+	fmt.Println(numberOfUser)
+
 }
