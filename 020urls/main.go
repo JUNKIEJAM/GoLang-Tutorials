41d7cb38e098070bb7db4889c17d8cbb4627d7ac
Prithesh Dwivedi <66085589+JUNKIEJAM@users.noreply.github.com>
2024-06-24 17:32:09 +0530

    Handling URLs

---
+++ 020urls/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+const myurl string = "https://www.google.com"
+
+func main() {
+	fmt.Println("Welcome to handling URLs in golang")
+	fmt.Println(myurl)
+
+	// parsing
+	result, _ := url.Prase(myurl)
+
+	// fmt.Println(result.Scheme);
+	// fmt.Println(result.Host);
+	// fmt.Println(result.Path);
+	// fmt.Println(result.Port());
+	// fmt.Println(result.RawQuery);
+
+	qparams := result.Query()
+	fmt.Println("The type of query params are: %T\n", qparams)
+
+}
