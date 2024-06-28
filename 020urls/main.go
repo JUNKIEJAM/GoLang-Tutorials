0f0a706ed4b92ba5247beb99fb8856956d41bc6b
prithesh.dwivedi <prithesh33vvv@gmail.com>
2024-06-28 09:34:21 +0530

    Handling URLs

--- 020urls/main.go
+++ 020urls/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/url"
+)
 
 const myurl string = "https://www.google.com"
 
@@ -9,15 +12,28 @@ func main() {
 	fmt.Println(myurl)
 
 	// parsing
-	result, _ := url.Prase(myurl)
+	result, _ := url.Parse(myurl)
 
-	// fmt.Println(result.Scheme);
-	// fmt.Println(result.Host);
-	// fmt.Println(result.Path);
-	// fmt.Println(result.Port());
+	fmt.Println(result.Scheme)
+	fmt.Println(result.Host)
+	fmt.Println(result.Path)
+	fmt.Println(result.Port())
 	fmt.Println(result.RawQuery)
 
 	qparams := result.Query()
 	fmt.Println("The type of query params are: %T\n", qparams)
+
+	for _, val := range qparams {
+		fmt.Println("Para is : ", val)
+	}
+
+	partsOfUrl := &url.URL{
+		Scheme:  "https",
+		Host:    "lco.dev",
+		Path:    "/tutcss",
+		RawPath: "user=hitesh"}
+
+	anotherURL := partsOfUrl.String()
+	fmt.Println(anotherURL)
 
 }
